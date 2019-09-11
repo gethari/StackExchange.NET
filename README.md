25f46e65677b7a0350da25381f10e679f672c322
Hariharan Subramanian <hariharan.subramanian@biztalk360.co.uk>
2019-09-11 12:43:31 +0530

    Added Readme

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-# FluentExchange
+# StackExchange.NET
+
+```
+ _____ _             _    ______          _                              _   _ ______ _______ 
+ / ____| |           | |  |  ____|        | |                            | \ | |  ____|__   __|
+| (___ | |_ __ _  ___| | _| |__  __  _____| |__   __ _ _ __   __ _  ___  |  \| | |__     | |   
+ \___ \| __/ _` |/ __| |/ /  __| \ \/ / __| '_ \ / _` | '_ \ / _` |/ _ \ | . ` |  __|    | |   
+ ____) | || (_| | (__|   <| |____ >  < (__| | | | (_| | | | | (_| |  __/_| |\  | |____   | |   
+|_____/ \__\__,_|\___|_|\_\______/_/\_\___|_| |_|\__,_|_| |_|\__, |\___(_)_| \_|______|  |_|   
+                                                              __/ |                            
+                                                             |___/                             
+ 
+```
+Created by Hari Haran
+- [StackExchange.NET](#stackexchangenet)
+  - [Overview](#overview)
+  - [Contributers](#contributers)
+  - [Usage](#usage)
+  - [Parameter Filters](#parameter-filters)
+
+## Overview
+StackExchange.NET is a .NET Standard managed library that provides easy access to the StackExchange APi's with virtually no boilerplate code required.
+
+Currently the library supports 18 Endpoints ( As on 11:09:2019) listed in the [API Docs](https://api.stackexchange.com/docs). I will be covering all the other endpoints as well, except those that require Auth.
+
+StackEchange.NET is FOSS (MIT License) and written entirely in c#.
+
+## Contributers
+
+## Usage
+
+To create a new API instance provide the apikey. If you need a new key check [docs](https://stackapps.com/apps/oauth/register)
+
+```
+  var client = new StackExchangeClient("yourApiKey");
+```
+
+You can easily find the relevant method under your preferred section. For example, lets say i want to `GetAllPosts` It can be simply achived by
+
+```
+var posts = client.Posts.GetAllPosts(postFilter);
+```
+
+Noticed the `postFiler` parameter. Similar to that, each method has its own parameter to filter the results from the api. The `Post Filter` object can also be accessed like this 
+
+```
+var postFilter = new PostFilter()
+{
+    Sort = PostSort.Creation
+;
+```
+
+## Parameter Filters
+
+There are different types of parameter filter objects available. Each `Parent Method` will have its own filter. Example : All the `clients.Comments` will accept a `CommentFilter()`
+
+- AnswerFilters
+- BadgeFilters
+- CommentFilter
+- PostFilter
+
+More to be added soon...
--- Sample/StackExchange.NET_Example/Program.cs
+++ Sample/StackExchange.NET_Example/Program.cs
@@ -6,82 +6,82 @@ using StackExchange.NET.Models;
 
 namespace StackExchange.NET_Example
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			var client = new StackExchangeClient("U4DMV*8nvpm3EOpvf69Rxw((");
-			var queryString = new AnswerFilters()
-			{
-				PageSize = 1,
-				Page = 1,
-				Sort = Sort.Votes
-			};
-			//var answers = client.Answers.GetAllAnswers(queryString);
-			var ids = new List<string>()
-			{
-				"44164379","6841479"
-			};
-			//var answers = client.Answers.GetAnswerByIds(ids, queryString);
-			//var answers = client.Answers.GetCommentsByIds(ids, queryString);
-			//var answers = client.Answers.GetQuestionByAnswerIds(ids, queryString);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new StackExchangeClient("U4DMV*8nvpm3EOpvf69Rxw((");
+            var queryString = new AnswerFilters()
+            {
+                PageSize = 1,
+                Page = 1,
+                Sort = Sort.Votes
+            };
+            //var answers = client.Answers.GetAllAnswers(queryString);
+            var ids = new List<string>()
+            {
+                "44164379","6841479"
+            };
+            //var answers = client.Answers.GetAnswerByIds(ids, queryString);
+            //var answers = client.Answers.GetCommentsByIds(ids, queryString);
+            //var answers = client.Answers.GetQuestionByAnswerIds(ids, queryString);
 
-			var badgeFilter = new BadgeFilters()
-			{
-				Sort = BadgeSort.Name
-			};
-			//var badges = client.Badges.GetAllBadges(badgeFilter);
+            var badgeFilter = new BadgeFilters()
+            {
+                Sort = BadgeSort.Name
+            };
+            //var badges = client.Badges.GetAllBadges(badgeFilter);
 
-			var batchIds = new List<string>()
-			{
-				"4600","2600","26"
-			};
+            var batchIds = new List<string>()
+            {
+                "4600","2600","26"
+            };
 
-			//var getBadgesByIds = client.Badges.GetBadgesByIds(batchIds, badgeFilter);
-			//var getBadgesByIds = client.Badges.GetRecentlyAwardedBadges(badgeFilter);
+            //var getBadgesByIds = client.Badges.GetBadgesByIds(batchIds, badgeFilter);
+            //var getBadgesByIds = client.Badges.GetRecentlyAwardedBadges(badgeFilter);
 
-			//var getBadgesByIds = client.Badges.GetRecentlyAwardedBadgesByIds(batchIds, badgeFilter);
+            //var getBadgesByIds = client.Badges.GetRecentlyAwardedBadgesByIds(batchIds, badgeFilter);
 
-			//var getBadgesByIds = client.Badges.GetAllTaggedBadges(badgeFilter);
-			//Console.WriteLine(JsonConvert.SerializeObject(getBadgesByIds));
+            //var getBadgesByIds = client.Badges.GetAllTaggedBadges(badgeFilter);
+            //Console.WriteLine(JsonConvert.SerializeObject(getBadgesByIds));
 
-			var commentFilter = new CommentFilter()
-			{
-				Sort = CommentSort.Creation
-			};
-			//var comments = client.Comments.GetAllComments(commentFilter);
+            var commentFilter = new CommentFilter()
+            {
+                Sort = CommentSort.Creation
+            };
+            //var comments = client.Comments.GetAllComments(commentFilter);
 
-			var commentIds = new List<string>()
-			{
-				"102165885", "102166303"
-			};
-			//var comments = client.Comments.GetCommentsByIds(commentIds,commentFilter);
-			//Console.WriteLine(JsonConvert.SerializeObject(comments));
+            var commentIds = new List<string>()
+            {
+                "102165885", "102166303"
+            };
+            //var comments = client.Comments.GetCommentsByIds(commentIds,commentFilter);
+            //Console.WriteLine(JsonConvert.SerializeObject(comments));
 
-			var postFilter = new PostFilter()
-			{
-				Sort = PostSort.Creation
-			};
-			//var posts = client.Posts.GetAllPosts(postFilter);
+            var postFilter = new PostFilter()
+            {
+                Sort = PostSort.Creation
+            };
+            //var posts = client.Posts.GetAllPosts(postFilter);
 
-			var postIds = new List<string>()
-			{
-				"57871119", "57698255"
-			};
+            var postIds = new List<string>()
+            {
+                "57871119", "57698255"
+            };
 
-			//var postsByIds = client.Posts.GetAllPostsByIds(postIds, postFilter);
+            var postsByIds = client.Posts.GetAllPostsByIds(postIds, postFilter);
 
-			//var postsByIds = client.Posts.GetCommentsOnPosts(postIds, postFilter);
+            //var postsByIds = client.Posts.GetCommentsOnPosts(postIds, postFilter);
 
-			//var revisionByIds = client.Posts.GetRevisionsByIds(postIds, postFilter);
+            //var revisionByIds = client.Posts.GetRevisionsByIds(postIds, postFilter);
 
-			var suggestedEdits = client.Posts.GetSuggestedEdits(postIds, new SuggestedEditFilter()
-			{
-				Sort = Sort.Activity
-			});
+            // var suggestedEdits = client.Posts.GetSuggestedEdits(postIds, new SuggestedEditFilter()
+            // {
+            // 	Sort = Sort.Activity
+            // });
 
-			Console.WriteLine(JsonConvert.SerializeObject(suggestedEdits));
-			Console.ReadKey();
-		}
-	}
+            //Console.WriteLine(JsonConvert.SerializeObject(suggestedEdits));
+            Console.ReadKey();
+        }
+    }
 }
